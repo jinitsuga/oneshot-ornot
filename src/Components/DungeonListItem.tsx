bc486d97e12bbb1ba09c8c3d9e89d9007c4d47b8
agu <iba.agustin@gmail.com>
2023-03-26 18:27:53 -0300

    split boss data into new component. initial setup

--- src/Components/DungeonListItem.tsx
+++ src/Components/DungeonListItem.tsx
@@ -2,28 +2,46 @@ import React, { FC } from "react";
 
 interface DungeonData {
   dungeon: any;
+  bossSelect: any;
 }
 
 const DungeonListItem: FC<DungeonData> = ({ dungeon }) => {
-  const awway = Object.entries(dungeon);
+  const [dungeonSelected, setDungeonSelect] = React.useState<boolean>(false);
 
-  const bossesAbilities = Object.entries(awway[2][1]);
+  const entries = Object.entries(dungeon);
 
-  console.log(bossesAbilities);
+  const bossesAndAbilities = Object.entries(entries[2][1]);
+  console.log(bossesAndAbilities);
 
-  bossesAbilities.map((item) => {
-    const abilities = Object.entries(item[1]);
-    console.log(
-      abilities.map(
-        (spell) => `${spell[0]} => ${spell[1].type}: ${spell[1].damage}`
-      )
-    );
+  const bossNames = bossesAndAbilities.map((boss, id) => {
+    const selectBoss = () => {};
+
+    return <li key={id}>{boss[0]}</li>;
   });
 
+  // bossesAndAbilities.map((item) => {
+  //   const abilities = Object.entries(item[1]);
+  //   console.log(
+  //     abilities.map(
+  //       (spell) => `${spell[0]} => ${spell[1].type}: ${spell[1].damage}`
+  //     )
+  //   );
+  // });
+
   return (
-    <div className="flex flex-row items-center justify-between w-64 border-emphasis p-2 m-2 border-4">
-      <button className="bg-yello">{dungeon.name}</button>
-      <img className="w-20 h-12" src={dungeon.image} />
+    <div className="flex flex-col items-center justify-between w-64 border-emphasis p-2 m-2 border-4">
+      <div className="flex flex-row items-center justify-around w-full">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+          className="bg-yello"
+        >
+          {dungeon.name}
+        </button>
+        <img className="w-20 h-12" src={dungeon.image} />
+      </div>
+      <ul>{bossNames}</ul>
     </div>
   );
 };
---
+++ src/Components/bossItem.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface Props {
+  abilities: any;
+}
+
+const Boss: FC<Props> = ({ abilities }) => {
+  return <div></div>;
+};
+
+export { Boss };
